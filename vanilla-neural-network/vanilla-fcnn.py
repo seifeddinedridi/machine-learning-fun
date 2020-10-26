b00c63a4abd5f6e8c9daca6207902b2ecc116732
Seifeddine <seifeddine.dridi89@gmail.com>
2020-10-26 17:32:52 +0100

    Testing against the MNIST dataset

--- vanilla-neural-network/vanilla-fcnn.py
+++ vanilla-neural-network/vanilla-fcnn.py
@@ -1,16 +1,15 @@
-import math
-
 import numpy as np
 from numpy import random
+from tqdm import trange
 
 np.random.seed(1337)
 
 
 # load the mnist dataset
 def fetch(url):
-    import requests, gzip, os, numpy
-    fp = os.path.join(os.getcwd(), "tmp", url)
-    if os.path.isfile(fp):
+    import requests, gzip, os, hashlib, numpy
+    fp = os.path.join("C:\\temp", hashlib.md5(url.encode('utf-8')).hexdigest())
+    if os.path.exists(fp):
         with open(fp, "rb") as f:
             dat = f.read()
     else:
@@ -20,11 +19,26 @@ def fetch(url):
     return numpy.frombuffer(gzip.decompress(dat), dtype=np.uint8).copy()
 
 
+def softmax_grad(X):
+    # Reshape the 1-d softmax to 2-d so that np.dot will do the matrix multiplication
+    s = X.reshape(-1, 1)
+    return np.diagflat(s) - np.dot(s, s.T)
+
+
+def cross_entropy_loss(predictions_Y, train_Y):
+    predictions_count = predictions_Y.shape[0]
+    scores = predictions_Y[range(predictions_count), train_Y]
+    with np.errstate(divide='ignore'):
+        log_likelihood = -np.where(scores > 0.0000001, np.log(scores), np.log(np.full(scores.shape, 0.00001)))
+        log_likelihood[np.isneginf(log_likelihood)] = -1e9
+    return np.sum(log_likelihood) / predictions_count
+
+
 class NNLayer(object):
     def __init__(self, size, activation_func='relu'):
         # self.W = np.random.randn(size[0], size[1]) * math.sqrt(2.0 / size[1])
         self.W = np.random.uniform(-1.0, 1.0, size) / np.sqrt(size[0] * size[1]).astype(np.float32)
-        self.bias = np.zeros(size[0])
+        self.bias = np.random.random(size[0]) * 0.01
         self.activation_func = activation_func
         self.X = np.zeros(size[1])
         self.Z = np.zeros(size[0])
@@ -32,12 +46,9 @@ class NNLayer(object):
         self.dL_dW = np.zeros(size)
         self.dL_dB = np.zeros(size[0])
 
-    def forward(self, X, delta_weight=None):
+    def forward(self, X):
         self.X = X
-        weights = self.W
-        if delta_weight is not None:
-            weights += delta_weight
-        self.Z = np.dot(weights, self.X) + self.bias
+        self.Z = np.dot(self.W, self.X) + self.bias
         self.Sigma = self.apply_activation(self.Z)
         return self.Sigma
 
@@ -50,15 +61,8 @@ class NNLayer(object):
         else:
             return 0.0
 
-    def softmax_grad(self, X):
-        # Reshape the 1-d softmax to 2-d so that np.dot will do the matrix multiplication
-        s = X.reshape(-1, 1)
-        return np.diagflat(s) - np.dot(s, s.T)
-
     def backward(self, dL_dSigmas):
         # This is a batch operation that is done for all the neurons in the current layer
-        dL_dX = np.zeros(self.X.shape[0])
-
         dZ_dX = self.W
         dZ_dW = self.X
         dZ_dB = np.ones(self.bias.shape)
@@ -69,25 +73,21 @@ class NNLayer(object):
             dSigma_dZ = np.diag(np.heaviside(self.Z, 1.0))
         elif self.activation_func == 'softmax':
             # Todo (Check correctness)
-            dSigma_dZ = self.softmax_grad(self.Sigma)
+            dSigma_dZ = softmax_grad(self.Sigma)
 
         dSigma_dX = np.dot(dSigma_dZ, dZ_dX)
         dSigma_dW = np.dot(dSigma_dZ, np.vstack([dZ_dW] * self.W.shape[0]))
-        dSigma_dB = np.dot(dSigma_dZ, dZ_dB)
+        dSigma_dB = np.dot(dSigma_dZ, np.vstack([dZ_dB] * self.W.shape[0]))
 
         dL_dX = np.dot(dL_dSigmas, dSigma_dX)
         dL_dW = np.multiply(dL_dSigmas[:, np.newaxis], dSigma_dW)
+        # dL_dW = np.vstack(dL_dSigmas) * dSigma_dW.sum(axis=0)
+        dL_dB = dL_dSigmas * dSigma_dB.sum(axis=0)
+        assert dL_dX.shape == self.X.shape
+        assert self.dL_dW.shape == dL_dW.shape
+        assert self.dL_dB.shape == dL_dB.shape
         self.dL_dW += dL_dW
-        self.dL_dB += np.dot(dL_dSigmas, dSigma_dB)
-
-        # for neuron_index, dL_dSigma in enumerate(dL_dSigmas):
-        #     dL_dXi = dL_dSigma * dSigma_dX[neuron_index]
-        #     dL_dWi = dL_dSigma * dSigma_dW[neuron_index]
-        #     dL_dX += dL_dXi
-        #
-        #     self.dL_dW[neuron_index] += dL_dWi
-        #     self.dL_dB[neuron_index] += dL_dSigma * dSigma_dB[neuron_index]
-
+        self.dL_dB += dL_dB
         return dL_dX
 
     def update(self, learning_rate):
@@ -103,40 +103,38 @@ class NNLayer(object):
 
 class VanillaFCNN(object):
 
-    def __init__(self, training_X, training_Y):
+    def __init__(self, training_X, training_Y, input_output):
         self.layers = []
         self.training_X = training_X
         self.training_Y = training_Y
+        self.input_output = input_output
 
     def addLayer(self, layer):
         self.layers.append(layer)
 
-    def forward(self, X, delta_weight=None, target_layer_idx=-1):
-        for layer_idx, layer in enumerate(self.layers):
-            if target_layer_idx == layer_idx:
-                X = layer.forward(X, delta_weight)
-            else:
-                X = layer.forward(X)
+    def forward(self, X):
+        for layer in self.layers:
+            X = layer.forward(X)
         return X
 
-    def cross_entropy_loss(self, predictions_Y):
-        predictions_count = predictions_Y.shape[0]
-        l = predictions_Y[range(predictions_count), self.training_Y.argmax(axis=1)]
-        with np.errstate(divide='ignore'):
-            log_likelihood = -np.where(l > 0.0000001, np.log(l), np.log(np.full(l.shape, 0.00001)))
-            log_likelihood[np.isneginf(log_likelihood)] = -1e9
-        return np.sum(log_likelihood) / predictions_count
-
-    def train(self, epochs=1, learning_rate=0.001):
+    def train(self, max_epoch, learning_rate, batch_size):
         loss = 0.0
-        for t in range(epochs):
-            predictions_Y = np.zeros(self.training_Y.shape, dtype=float)
-            for input_index, Xtr in enumerate(self.training_X):
+        t = trange(max_epoch)
+        for i in t:
+            # Create batch
+            sample = np.random.randint(0, self.training_X.shape[0], size=batch_size)
+            batch_train_X = self.training_X[sample].reshape((-1, 28 * 28))
+            batch_train_Y = self.training_Y[sample]
+
+            predictions_Y = np.zeros((batch_train_Y.shape[0], self.input_output[1]), dtype=np.float32)
+            for input_index, Xtr in enumerate(batch_train_X):
                 predictions_Y[input_index] = self.forward(Xtr)
 
                 # Backpropagation
                 # Start with the derivative of the Loss function wrt. last layer's output
-                dL_dX = predictions_Y[input_index] - self.training_Y[input_index]
+                dL_dX = predictions_Y[input_index].copy()
+                dL_dX[batch_train_Y[input_index]] -= 1.0
+
                 for layer in reversed(self.layers):
                     dL_dX = layer.backward(dL_dX)
 
@@ -144,74 +142,57 @@ class VanillaFCNN(object):
             for layer in self.layers:
                 layer.update(learning_rate)
 
-            loss += self.cross_entropy_loss(predictions_Y)
-            accuracy = np.mean(np.argmax(predictions_Y, axis=1) == np.argmax(self.training_Y, axis=1))
-            print('Epoch %d / %d, Loss = %f, Accuracy = %.2f' % (t + 1, epochs, loss / (t + 1), accuracy.mean()))
+            loss += cross_entropy_loss(predictions_Y, batch_train_Y)
+            accuracy = np.mean(np.argmax(predictions_Y, axis=1) == batch_train_Y)
+            t.set_description(
+                'Epoch %d / %d, Loss = %f, Accuracy = %.2f' % (i + 1, max_epoch, loss / (i + 1), accuracy.mean()))
 
-        return loss / epochs
+        return loss / max_epoch
 
     def get_neuron_gradient_vector(self, layer_index, neuron_index):
         return self.layers[layer_index].get_neuron_gradient_vector(neuron_index)
 
-    def compute_analytical_gradient(self, loss, layer_index, neuron_index, delta):
-        print('Base Loss value: ', loss)
-
-        dW = np.zeros(self.layers[layer_index].W.shape)
-        dL_dW = np.zeros(self.layers[layer_index].W.shape[1])
-
-        # Given a selected neuron, we are differentiating wrt. each of its input connections
-        for input_connection_index in range(self.layers[layer_index].W.shape[1]):
-            dW[neuron_index][input_connection_index] = delta
-
-            predictions_Y = np.zeros(self.training_Y.shape, dtype=float)
-            for input_index, Xtr in enumerate(self.training_X):
-                predictions_Y[input_index] = self.forward(Xtr, dW, layer_index)
-
-            delta_loss = self.cross_entropy_loss(predictions_Y) - loss
-            dL_dW[input_connection_index] = delta_loss / (delta * self.training_X.shape[0])
-
-            dW[neuron_index][input_connection_index] = 0.0
-
-        return dL_dW
-
-
-def generateTrainingDataset(inputSize, outputSize, datasetSize):
-    xTrs = np.random.random((datasetSize, inputSize))
-    yTrs = np.zeros((datasetSize, outputSize))
-    batch_size = int(datasetSize / 3)
-    for y in range(datasetSize):
-        idx = random.choice(range(outputSize))
-        yTrs[y, idx % outputSize] = 1.0
-        # yTrs[y, 0] = 1.0
-    return xTrs, yTrs
+    # def compute_analytical_gradient(self, loss, layer_index, neuron_index, delta):
+    #     print('Base Loss value: ', loss)
+    #
+    #     dW = np.zeros(self.layers[layer_index].W.shape)
+    #     dL_dW = np.zeros(self.layers[layer_index].W.shape[1])
+    #
+    #     # Given a selected neuron, we are differentiating wrt. each of its input connections
+    #     for input_connection_index in range(self.layers[layer_index].W.shape[1]):
+    #         dW[neuron_index][input_connection_index] = delta
+    #
+    #         # TODO fixme: This code is broken
+    #         predictions_Y = np.zeros(self.training_Y.shape, dtype=float)
+    #         for input_index, Xtr in enumerate(self.training_X):
+    #             predictions_Y[input_index] = self.forward(Xtr, dW, layer_index)
+    #
+    #         delta_loss = self.cross_entropy_loss(predictions_Y) - loss
+    #         dL_dW[input_connection_index] = delta_loss / (delta * self.training_X.shape[0])
+    #
+    #         dW[neuron_index][input_connection_index] = 0.0
+    #
+    #     return dL_dW
 
 
 def create_neural_network():
+    X_train = fetch("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28 * 28)) / 255.0
+    Y_train = fetch("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz")[8:]
+    X_test = fetch("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28 * 28)) / 255.0
+    Y_test = fetch("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")[8:]
+
     inputShapeSize = 784
     outputShapeSize = 10
-
-    # Generate training dataset
-    trainingDatasetSize = 100
-    # X_train, Y_train = generateTrainingDataset(inputShapeSize, outputShapeSize, trainingDatasetSize)
-
-    X_train = fetch("train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 784))[0:trainingDatasetSize, ]
-    Y_train = fetch("train-labels-idx1-ubyte.gz")[8:][0:trainingDatasetSize]
-    X_test = fetch("t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 784))
-    Y_test = fetch("t10k-labels-idx1-ubyte.gz")[8:]
-
-    # Convert Y_train and Y_test into hot vectors
-    Y_train_1hv = np.zeros((Y_train.size, Y_train.max() + 1))
-    Y_train_1hv[np.arange(Y_train.size), Y_train] = 1
-
-    fcnn = VanillaFCNN(X_train, Y_train_1hv)
+    fcnn = VanillaFCNN(X_train, Y_train, (inputShapeSize, outputShapeSize))
     fcnn.addLayer(NNLayer((128, inputShapeSize), 'relu'))
     fcnn.addLayer(NNLayer((outputShapeSize, 128), 'softmax'))
     return fcnn
 
+
+network_parameters = {'delta': 0.001, 'epochs': 1000, 'batch_size': 128}
+
 fcnn = create_neural_network()
-delta = 0.001
-epochs = 1000
-loss = fcnn.train(epochs, delta)
+loss = fcnn.train(network_parameters['epochs'], network_parameters['delta'], network_parameters['batch_size'])
 print('Estimated loss = %f' % loss)
 
 # analytical_gradient = fcnn.compute_analytical_gradient(loss, 1, 0, delta)
